b5b60ffe70a975ef0257d52f1323243a4cf4a948
Arnaud <arnaudralec@gmail.com>
2022-10-22 17:13:24 +0200

    _

--- vuereflexes/src/App.vue
+++ vuereflexes/src/App.vue
@@ -34,14 +34,17 @@ export default {
   <div class="ui centered container">
     <div class="ui raised very padded text container segment">
       <div class="ui padded grid">
+
         <div class="sixteen wide column">
           <h2 class="ui header">Reaction timer</h2>
           <div class="ui primary button" @click="start" :disabled="ongoing">play</div>
         </div>
+
         <div class="sixteen wide column">
           <Target v-if="ongoing" :delay="delay" @fired="finish"/>
           <Results v-if="showResults" :score="score"/>
         </div>
+
       </div>
     </div>
   </div>
